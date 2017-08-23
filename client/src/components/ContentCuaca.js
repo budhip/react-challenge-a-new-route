30336123c6232a691da012fed909adc981165d27
budhip <ryo.bunraku@yahoo.com>
2017-08-23 08:11:23 +0700

    menambahkan form input cek cuaca berdasarkan kota

--- client/src/components/ContentCuaca.js
+++ client/src/components/ContentCuaca.js
@@ -11,9 +11,16 @@ class ContentCuaca extends Component {
       kelembaban: '',
       suhu: '',
       cuaca: '',
-      icon: ''
+      icon: '',
+      weatherList: [],
+      cityInput: ''
     }
+
+    this.getWeather = this.getWeather.bind(this)
+    this.gantiKota = this.gantiKota.bind(this)
+    this.cariCuaca = this.cariCuaca.bind(this)
   }
+
   getWeather (data) {
     this.setState({
       kota: data.name,
@@ -31,6 +38,39 @@ class ContentCuaca extends Component {
       this.getWeather(response.data)
     })
     .catch(err => console.log(err))
+    axios.get('http://api.openweathermap.org/data/2.5/forecast?q=Jakarta&APPID=80acb29daded6e808231e31fb1c6666b')
+    .then(response => {
+      this.setState({
+        weatherList: response.data.list
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  cariCuaca (e) {
+    e.preventDefault()
+    let kotaPilihan = this.state.cityInput
+    axios.get('http://api.openweathermap.org/data/2.5/weather?q=' + kotaPilihan + '&APPID=80acb29daded6e808231e31fb1c6666b')
+    .then(response => {
+      this.getWeather(response.data)
+      this.setState({
+        cityInput: ''
+      })
+    })
+    .catch(err => console.log(err))
+    axios.get('http://api.openweathermap.org/data/2.5/forecast?q=' + kotaPilihan + '&APPID=80acb29daded6e808231e31fb1c6666b')
+    .then(response => {
+      this.setState({
+        weatherList: response.data.list
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  gantiKota (data) {
+    this.setState({
+      cityInput: data
+    })
   }
 
   render = () => {
@@ -38,6 +78,14 @@ class ContentCuaca extends Component {
     return(
       <div>
         <Header />
+        <div className="rows">
+          <div className="col-md-4">
+            <form onSubmit={this.cariCuaca}>
+              <label className="label">Nama Kota:</label>&nbsp;
+                <input className="input" type="text" placeholder="--Nama kota--" onChange={(e) => this.gantiKota(e.target.value)} value={this.state.cityInput}/>
+            </form>
+          </div>
+        </div>
         <h1 className=""> Cuaca hari ini </h1>
         <div className="card border-success mb-3" style={{width: '20rem'}}>
           <div className="card-header bg-warning border-success">{this.state.kota}</div>
@@ -49,7 +97,7 @@ class ContentCuaca extends Component {
           <div className="card-footer border-success">Sumber: OpenWeatherMap</div>
         </div>
         <hr />
-        <MainForecast />
+        <MainForecast weatherList={this.state.weatherList}/>
       </div>
     );
   }
--- client/src/components/MainForecast.js
+++ client/src/components/MainForecast.js
@@ -1,37 +1,13 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import ForecastItem from './ForecastItem';
 
 class MainForecast extends Component{
-  constructor () {
-    super()
-    this.state = {
-      weatherList: []
-    }
-    this.getWeathers = this.getWeathers.bind(this)
-  }
-
-  getWeathers (data) {
-    this.setState({
-      weatherList: data
-    })
-  }
-
-  componentWillMount () {
-    axios.get('http://api.openweathermap.org/data/2.5/forecast?q=Jakarta&APPID=80acb29daded6e808231e31fb1c6666b')
-    .then(response => {
-      console.log('ini MainForecast',response.data.list);
-      this.getWeathers(response.data.list)
-    })
-    .catch(err => console.log(err))
-  }
-
   render = () => {
     return(
       <div>
-        <h1>Perkiraan cuaca 5 hari kedepan</h1>
+        <h1>Perkiraan Minggu Ini</h1>
         <div className="row">
-          {this.state.weatherList.map((weather, index) => (
+          {this.props.weatherList.map((weather, index) => (
             (weather.dt_txt.includes("06:00:00")) ? <ForecastItem
             cuaca = {
               weather.weather[0].description
